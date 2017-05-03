d9d9363e5b56d45fc97ce32750e61faf02aaf91d
C13490888 <c13490888@mydit.ie>
2017-05-03 14:46:08 +0000

    adding corsDD

--- webm_app/admin.py
+++ webm_app/admin.py
@@ -1,4 +1,8 @@
 from django.contrib.gis import admin
 from .models import WorldBorder
+from .models import PayAndDisplayMachine
+from .models import PayAndDisplayOutlet
 
 admin.site.register(WorldBorder, admin.GeoModelAdmin)
+admin.site.register(PayAndDisplayMachine, admin.GeoModelAdmin)
+admin.site.register(PayAndDisplayOutlet, admin.GeoModelAdmin)
